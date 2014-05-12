08d2405f5220344ef30faad39ddf97dc227e3f1d
Chris Uehlinger <chris.uehlinger@gmail.com>
2014-05-12 17:00:57 -0400

    Built the app

--- js/application.js
+++ js/application.js
@@ -1,4 +1,9 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  var ai = new AI();
+  // var inputManager = new KeyboardInputManager();
+  var actuator = new HTMLActuator();
+  var manager = new GameManager(4, ai, actuator, LocalStorageManager);
+  setTimeout(function(){ai.run(manager.game);}, 100);
+
 });
